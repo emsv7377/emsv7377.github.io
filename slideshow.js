3861ca40dc242c762aac23cf483bfd0250171ad2
emsv7377 <emmy.svensson.7377@student.uu.se>
2023-10-23 14:39:28 +0200

    more fix

--- slideshow.js
+++ slideshow.js
@@ -1,26 +1,34 @@
-let slideIndex = 1; // Initialize to the first image
+// Initialize slideIndex for each slideshow
+const slideIndexes = [1, 1, 1]; // Initialize to the first image for each slideshow
 
-function showSlides(n) {
-    const slides = document.querySelectorAll('.slideshow-container img');
+function showSlides(slideshowIndex, n) {
+    const slides = document.querySelectorAll(`.slideshow-container-${slideshowIndex} img`);
     if (n > slides.length) {
-        slideIndex = 1;
+        slideIndexes[slideshowIndex] = 1;
     }
     if (n < 1) {
-        slideIndex = slides.length;
+        slideIndexes[slideshowIndex] = slides.length;
     }
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
     }
-    slides[slideIndex - 1].style.display = 'block';
+    slides[slideIndexes[slideshowIndex] - 1].style.display = 'block';
 }
 
-function plusSlides(n) {
-    showSlides((slideIndex += n));
+function plusSlides(slideshowIndex, n) {
+    showSlides(slideshowIndex, (slideIndexes[slideshowIndex] += n));
 }
 
-showSlides(slideIndex); // Display the initial image
+// Call showSlides for each slideshow
+showSlides(0, slideIndexes[0]); // Display the initial image for the first slideshow
+showSlides(1, slideIndexes[1]); // Display the initial image for the second slideshow
+showSlides(2, slideIndexes[2]); // Display the initial image for the third slideshow
 
-// Start the slideshow when the page loads
+// Start the slideshows when the page loads
 window.onload = function () {
-    showSlides(slideIndex);
+    showSlides(0, slideIndexes[0]); // Start the first slideshow
+    showSlides(1, slideIndexes[1]); // Start the second slideshow
+    showSlides(2, slideIndexes[2]); // Start the third slideshow
 };
+
+
